defc72cbc9bd3121fd83440fbbb5fd886e5c535a
Weißer Hase <wei3erHase@protonmail.com>
2024-08-06 13:48:17 +0200

    feat: deprecating eth-sdk in favour of JSON abis

--- src/fetch-strategy.ts
+++ src/fetch-strategy.ts
@@ -1,25 +1,10 @@
-import {getMainnetSdk} from '@dethcrypto/eth-sdk-client';
-import {providers, Wallet} from 'ethers';
-import dotenv from 'dotenv';
-import {getEnvVariable} from './utils/misc';
-
-dotenv.config();
-
-/* ==============================================================/*
-                          SETUP
-/*============================================================== */
-
-// environment variables usage
-const provider = new providers.WebSocketProvider(getEnvVariable('RPC_WSS_URI'));
-const txSigner = new Wallet(getEnvVariable('TX_SIGNER_PRIVATE_KEY'), provider);
-
-const {dataFeedJob: job, dataFeed} = getMainnetSdk(txSigner);
+import type {Contract} from 'ethers';
 
 /* ==============================================================/*
                       AVAILABLE POOLS
 /*============================================================== */
 
-export async function getAllWhitelistedSalts(): Promise<string[]> {
+export async function getAllWhitelistedSalts(dataFeed: Contract, job: Contract): Promise<string[]> {
   dataFeed.attach(await job.dataFeed()); // Enforces dataFeed to be the job's one
   const whitelistedSalts = await dataFeed.whitelistedPools();
   return whitelistedSalts;
--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -2,3 +2,21 @@
 export const PAST_BLOCKS = 14_400; // 2d
 
 export const SUPPORTED_CHAIN_IDS = [10, 137];
+
+type ContractDetails = {
+  job: string;
+  dataFeed: string;
+};
+
+type ContractsMapping = Record<number, ContractDetails>;
+
+export const contracts: ContractsMapping = {
+  1: {
+    job: '0x1f5f0DA9391AB08c7F0150d45B41F6900fb4Fd0C',
+    dataFeed: '0x1ce81290Eb4c10cC9Fa71256799665423e87b628',
+  },
+  11_155_111: {
+    job: '0x6c461C0296eBE3715820F1Cbde856219e06ac3B8',
+    dataFeed: '0x553365bdda2Fd60608Fb05CB7ad32620e3A126DD',
+  },
+};
